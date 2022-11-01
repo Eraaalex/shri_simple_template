94287091b0d96bc9c1b287378e2cdcc70ceea87b
Eralex <90034745+Eraaalex@users.noreply.github.com>
2022-11-01 12:50:54 +0300

    eighth sad commit

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ const config = {
                   ]
             },
             {
-                test: /\.(js|jsx)$/i,
+                test: /\.(js|jsx)$/,
                 loader: 'babel-loader',
                 options: {
                   presets: [
